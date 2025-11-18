09853d386067b503dc0d66fade568a9748a62c05
FadliRob <75844473+FadliRob@users.noreply.github.com>
2025-11-18 10:48:38 +0700

    Update import paths for Three.js and loaders

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import * as THREE from "./libs/three.module.js";
-import { GLTFLoader } from "./libs/GLTFLoader.js";
-import { RGBELoader } from "./libs/RGBELoader.js";
-import { PointerLockControls } from "./libs/PointerLockControls.js";
+import * as THREE from "/libs/three.min.js";
+import { GLTFLoader } from "/libs/GLTFLoader.js";
+import { RGBELoader } from "/libs/RGBELoader.js";
+import { PointerLockControls } from "/libs/PointerLockControls.js";
 
 let scene, camera, renderer, controls;
 let moveForward = false, moveBackward = false, moveLeft = false, moveRight = false;
@@ -89,3 +89,4 @@ function animate() {
 
     controls.moveRight(dir.x * speed);
     controls.moveForward(dir.z * speed);
+
